6a251c97c2081d0c63238cad2e5aed805a7d6a3c
mingsc <KevinChen.tw@gmail.com>
2018-07-11 11:58:46 +0800

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Ming hsien Chen on 2018/7/11.
+//  Copyright © 2018年 Ming hsien Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
